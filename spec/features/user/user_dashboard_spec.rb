38df20bbfac8c33a00aef0fb14ad2be9d03277c9
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-21 16:17:56 -0600

    Add github repos to users show action

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -7,11 +7,7 @@ RSpec.describe "Dashboard page" do
         user = create(:user, token: ENV['GITHUB_API_KEY'])
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-        # VCR.use_cassette('github/dashboard', record: :new_episodes) do
-          WebMock.allow_net_connect!
-          # VCR.eject_cassette
-          VCR.turn_off!
-
+        VCR.use_cassette('github/dashboard', record: :new_episodes) do
           visit dashboard_path
 
           within ".github" do
@@ -20,8 +16,9 @@ RSpec.describe "Dashboard page" do
             expect(page).to have_link("battleship")
             expect(page).to have_link("blogger")
             expect(page).to have_link("boat_rental")
+            expect(page).to_not have_link("1903_final")
           end
-        # end
+        end
       end
     end
   end
