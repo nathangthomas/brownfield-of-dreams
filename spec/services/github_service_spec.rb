1269bcf76289161e2d5ea0334a25f5909c489e38
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-22 11:36:57 -0600

    Removed vcr and webmock info. Test still passing

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe GithubService do
   context "instance methods" do
     context "#users_repos" do
       it "returns specific users data" do
-        WebMock.allow_net_connect!
-        VCR.turn_off!
-        repos = subject.users_repos(5)
-        expect(repos).to be_a Array
-        expect(repos.count).to eq 5
-        user_data = repos.first
+        VCR.use_cassette('github/dashboard', record: :new_episodes) do
 
-        expect(user_data).to have_key :id
-        expect(user_data).to have_key :name
-        expect(user_data).to have_key :url
-        expect(user_data).to have_key :html_url
+          repos = subject.users_repos(5)
+          expect(repos).to be_a Array
+          expect(repos.count).to eq 5
+          user_data = repos.first
+
+          expect(user_data).to have_key :id
+          expect(user_data).to have_key :name
+          expect(user_data).to have_key :url
+          expect(user_data).to have_key :html_url
+        end
       end
     end
   end
